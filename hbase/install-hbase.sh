a4310765fcad744703378ca32bdf47a1812cd358
Wheel Fireman <bogenbroom@gmail.com>
2017-05-11 01:14:39 -0400

    deleted hbase and simplified gitignore

--- hbase/install-hbase.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-wget --quiet http://mirror.nexcess.net/apache/hbase/1.1.5/hbase-1.1.5-bin.tar.gz
-tar xzf hbase-1.1.5-bin.tar.gz -C /opt/
-ln -s /opt/hbase-1.1.5 /opt/hbase
-
-rm hbase-1.1.5-bin.tar.gz
