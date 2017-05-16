3dfdb9c6b0872535446f7e9979caf1fa50c5d806
Wheel Fireman <willmforeman@gmail.com>
2017-05-16 16:06:46 -0400

    updated loginto script

--- loginto.sh
+++ loginto.sh
@@ -2,24 +2,28 @@
 echo "\n"
 
 PS3='Choose a container to log in to: '
-options=("Rstudio" "Hadoop" "Postgres" "Hive" "Cancel")
+options=("Rstudio" "Hadoop" "Postgres" "Hive" "Sparklyr" "Cancel" )
 select opt in "${options[@]}"
 do
     case $opt in
         "Rstudio")
-            docker exec -ti hadoopdocker_rstudio_1 bash
+            docker exec -ti rspark_rstudio_1 bash
             break
             ;;
         "Hadoop")
-            docker exec -ti hadoopdocker_hadoop_1 bash
+            docker exec -ti rspark_hadoop_1 bash
             break
             ;;
         "Postgres")
-            docker exec -ti hadoopdocker_postgres_1 bash
+            docker exec -ti rspark_postgres_1 bash
             break
             ;;
         "Hive")
-            docker exec -ti hadoopdocker_hive_1 bash
+            docker exec -ti rspark_hive_1 bash
+            break
+            ;;
+        "Sparklyr")
+            docker exec -ti rspark_sparklyrstudio_1 bash
             break
             ;;
         "Cancel")
