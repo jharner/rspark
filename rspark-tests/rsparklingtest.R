023967efee9e8d481c503c487f55e2098b2f81f4
Jim Harner <jharner@me.com>
2017-06-24 16:51:29 -0400

    loaded sparklyr before rsparkling; corrected code in rsparkling test

--- rspark-tests/rsparklingtest.R
+++ rspark-tests/rsparklingtest.R
@@ -2,13 +2,12 @@
 if (nchar(Sys.getenv("SPARK_HOME")) < 1) {
   Sys.setenv(SPARK_HOME = "/opt/spark")
 }
-  
-detach("package:rsparkling", unload = TRUE)
-if ("package:h2o" %in% search()) { detach("package:h2o", unload = TRUE) }
-if (isNamespaceLoaded("h2o")){ unloadNamespace("h2o") }
-remove.packages("h2o")
-install.packages("h2o", type = "source", 
-                 repos = "http://h2o-release.s3.amazonaws.com/h2o/rel-tverberg/2/R")
+
+# if ("h2o" %in% search()) { detach("h2o", unload = TRUE) }
+# if (isNamespaceLoaded("h2o")){ unloadNamespace("h2o") }
+# remove.packages("h2o")
+# install.packages("h2o", type = "source", 
+#                 repos = "http://h2o-release.s3.amazonaws.com/h2o/rel-tverberg/2/R")
 
 options(rsparkling.sparklingwater.version = "2.0.3")
 
@@ -22,7 +21,7 @@ sc <- spark_connect(master = "local")
 mtcars_tbl <- copy_to(sc, mtcars, "mtcars")
 mtcars_tbl
 
-as.h2o_frame(sc, mtcars_tbl, strict_version_check = FALSE)
+as_h2o_frame(sc, mtcars_tbl, strict_version_check = FALSE)
 
 # stop the SparkR session
 spark_disconnect(sc)
