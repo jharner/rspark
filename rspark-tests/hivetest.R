e270c6db8c3347a2502b361b17a0f9da8e5a54fc
Jim Harner <jharner@me.com>
2017-07-01 15:27:39 -0400

    minor edits to docker file; debugging edits to hivetest; added hadoop text file

--- rspark-tests/hivetest.R
+++ rspark-tests/hivetest.R
@@ -8,4 +8,5 @@ dbSendUpdate(con, "create table test1 (a int, b string)")
 dbSendUpdate(con, "insert into test1 (a,b) values (1, 'foo')")
 dbSendUpdate(con, "insert into test1 (a,b) values (2, 'bar')")
 
-print(dbReadTable(con, "test1"))
+# print(dbReadTable(con, "test1"))
+dbReadTable(con, "test1")
