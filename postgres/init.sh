21ab50822f04f27ede429b99482b60605ff42782
Mark Lilback <mark@lilback.com>
2017-09-23 13:28:54 -0400

    pg database created by dockerfile

---
+++ postgres/init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export PATH=/usr/lib/postgresql/9.4/bin:$PATH
+initdb -D /opt/pg-data
+pg_ctl -D /opt/pg-data start
+sleep 10
+psql --command "create user rstudio createdb"
+psql --command "create user hive"
+createdb -O rstudio rstudio
+createdb -O hive hive
+createdb -O rstudio dataexpo
+createdb -O rstudio testdb
+psql -U rstudio dataexpo < /opt/dataexpo.sql  > /dev/null
+service postgresql stop
+
