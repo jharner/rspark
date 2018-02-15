b570f3db27613bd3f45888fb8b1f28a0b61b70ad
Jim Harner <jharner@me.com>
2018-02-14 22:54:18 -0500

    spark cluster added

---
+++ startspark.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+while [[ $# -gt 0 ]]
+do
+key=$1
+
+case $key in
+	-b|--build|build)
+	shift
+	docker build -t jh-postgres postgres
+	docker build -t jh-hadoop hadoop
+	docker build -t jh-hive hive
+	docker build -t jh-rstudio rstudio
+	;;
+	-r|--reset)
+	echo "resetting data directory"
+	docker rm hadoop_hadoop_1 hadoop_hive_1 hadoop_postgres_1 hadoop_rstudio_1
+	rm -rf data/pg-data data/hadoop
+	shift
+	;;
+	*)
+	echo "unknown option $key"
+	exit 1
+	;;
+esac
+done 
+
+if [[ ! -e data ]]; then
+	mkdir data
+	# possibly Add Git pull here
+fi
+
+if [[ ! -e data/pg-data ]]; then
+	mkdir data/pg-data
+fi
+
+if [[ ! -e data/rstudio ]]; then
+	mkdir data/rstudio
+fi
+
+if [[ ! -e data/hadoop ]]; then
+	mkdir data/hadoop
+fi
+
+docker-compose -f spark-compose.yml up
