08f8bf7eb843869eae6dc6c470a885f7367a89b7
Chris Grant <chrisgrant.datascience@gmail.com>
2020-07-19 12:47:04 -0400

    dockill.sh now runs in a loop terminated by a quit signal (option 6).

--- dockill.sh
+++ dockill.sh
@@ -1,34 +1,38 @@
 #! /bin/sh
-echo "\n"
-
-PS3='Docker Image Manager: '
-options=("Stop Containers" "Delete Containers" "Delete Orphan Images" "Delete All Images" "Delete Hanging Volumes" "Quit")
-select opt in "${options[@]}"
+EXIT_CODE=0
+while [ $EXIT_CODE == 0 ]
 do
-    case $opt in
-        "Stop Containers")
-            docker stop $(docker ps -a -q)
-            break
-            ;;
-        "Delete Containers")
-            docker rm $(docker ps -a -q)
-            break
-            ;;
-        "Delete Orphan Images")
-            docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi
-            break
-            ;;
-        "Delete All Images")
-            docker rmi -f $(docker images -q)
-            break
-            ;;
-        "Delete Hanging Volumes")
-            docker volume rm $(docker volume ls -f dangling=true -q)
-            break
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo invalid option;;
-    esac
+  echo $'\n'
+  PS3='Docker Image Manager: '
+  options=("Stop Containers" "Delete Containers" "Delete Orphan Images" "Delete All Images" "Delete Hanging Volumes" "Quit")
+  select opt in "${options[@]}"
+  do
+      case $opt in
+          "Stop Containers")
+              docker stop $(docker ps -a -q)
+              break
+              ;;
+          "Delete Containers")
+              docker rm $(docker ps -a -q)
+              break
+              ;;
+          "Delete Orphan Images")
+              docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi
+              break
+              ;;
+          "Delete All Images")
+              docker rmi -f $(docker images -q)
+              break
+              ;;
+          "Delete Hanging Volumes")
+              docker volume rm $(docker volume ls -f dangling=true -q)
+              break
+              ;;
+          "Quit")
+              EXIT_CODE=1
+              break
+              ;;
+          *) echo invalid option;;
+      esac
+  done
 done
