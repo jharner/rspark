171631c6cc3c2622023e01f7916a6ba4d15a6a55
Chris Grant <chrisgrant.datascience@gmail.com>
2020-07-21 15:48:02 -0400

    Added the "Remove Unused Networks" option to the dockill.sh utility.

--- dockill.sh
+++ dockill.sh
@@ -4,7 +4,7 @@ while [ $EXIT_CODE == 0 ]
 do
   echo $'\n'
   PS3='Docker Image Manager: '
-  options=("Stop Containers" "Delete Containers" "Delete Orphan Images" "Delete All Images" "Delete Hanging Volumes" "Quit")
+  options=("Stop Containers" "Delete Containers" "Delete Orphan Images" "Delete All Images" "Delete Hanging Volumes" "Remove Unused Networks" "Quit")
   select opt in "${options[@]}"
   do
       case $opt in
@@ -26,6 +26,10 @@ do
               ;;
           "Delete Hanging Volumes")
               docker volume rm $(docker volume ls -f dangling=true -q)
+              break
+              ;;
+          "Remove Unused Networks")
+              docker network prune
               break
               ;;
           "Quit")
