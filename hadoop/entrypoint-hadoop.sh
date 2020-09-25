d6ae2b8843ee9a3c8df0e37def9c4d24ae2685de
Chris Grant <chrisgrant.datascience@gmail.com>
2020-09-25 01:09:49 -0400

    Made HADOOP_HOME the environment variable for hadoop (was HADOOP_PREFIX).

--- hadoop/entrypoint-hadoop.sh
+++ hadoop/entrypoint-hadoop.sh
@@ -7,18 +7,18 @@ echo "** Checking Postgres Connection **"
 
 sleep 10
 echo "starting run"
-: ${HADOOP_PREFIX:=/usr/local/hadoop}
+: ${HADOOP_HOME:=/opt/hadoop}
 
-chmod +x $HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
-$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
-DATA_DIR=/opt/hadoop
+chmod +x ${HADOOP_HOME}/etc/hadoop/hadoop-env.sh
+${HADOOP_HOME}/etc/hadoop/hadoop-env.sh
+DATA_DIR=/opt/hadoop-data
 export JAVA_HOME=/usr/lib/jvm/adoptopenjdk-8-hotspot-amd64
 
 rm /tmp/*.pid
 
 echo "installing libs"
 # installing libraries if any - (resource urls added comma separated to the ACP system variable)
-cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
+cd ${HADOOP_HOME}/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
 
 # altering the core-site configuration
 #sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
@@ -32,23 +32,23 @@ service ssh start
 
 chown -R rstudio ${DATA_DIR}
 
-echo "as rstudio using hadoop ${HADOOP_PREFIX}"
+echo "as rstudio using hadoop ${HADOOP_HOME}"
 CREATEDIR=0
 if [[ ! -d /${DATA_DIR}/nn ]]; then
 	rm -rf /${DATA_DIR}/nn /${DATA_DIR}/dfs
 	echo "running namenode init"
-	/usr/local/hadoop/bin/hdfs namenode -format -force
+	/opt/hadoop/bin/hdfs namenode -format -force
 	CREATEDIR=1
 	echo "namenode inited"
 fi
 
-$HADOOP_PREFIX/sbin/start-dfs.sh
+${HADOOP_HOME}/sbin/start-dfs.sh
 echo "started dfs"
-$HADOOP_PREFIX/sbin/start-yarn.sh
+${HADOOP_HOME}/sbin/start-yarn.sh
 echo "started yarn"
 
 #add hadoop bin directory to path
-PATH=/usr/local/hadoop/bin:$PATH
+PATH=/opt/hadoop/bin:$PATH
 #if we initialized namenode's file system, create default paths
 if [[ $CREATEDIR == 1 ]]; then
 	sleep 2
