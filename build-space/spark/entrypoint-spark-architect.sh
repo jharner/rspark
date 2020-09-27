86430566139424a29781bd1800f88852234c0da1
Chris Grant <chrisgrant.datascience@gmail.com>
2020-09-27 15:16:34 -0400

    Removed development directory.

--- build-space/spark/entrypoint-spark-architect.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/opt/spark/bin/spark-class org.apache.spark.deploy.master.Master
--- build-space/spark/entrypoint-spark-tecton.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/opt/spark/bin/spark-class org.apache.spark.deploy.worker.Worker spark://master:7077
