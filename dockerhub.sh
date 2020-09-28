d839b5629ebd36cc4d38a80336aa55e3f2f5b311
jharner <ejharner@gmail.com>
2020-09-28 00:47:25 -0400

    added shell script for dockerhub image pushes

---
+++ dockerhub.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+
+# docker login -u username -p password
+USERNAME="jharner"
+IMAGE=$1
+REPONAME=$USERNAME/$IMAGE
+TAG=1.0.0
+
+docker tag $IMAGE:latest $REPONAME:$TAG
+docker push $REPONAME:$TAG
